90ebf7d9fb249aefe839df43510859c3371e1df6
Auston Stewart <auston@shedbuilt.net>
2017-12-24 22:33:26 -0500

    Set default root password if unset

--- postinstall.sh
+++ postinstall.sh
@@ -2,3 +2,10 @@
 pwconv
 grpconv
 sed -i 's/yes/no/' /etc/default/useradd
+# Set root password to default if not set
+read -ra PASSWDSTAT <<< $(passwd --status root) || exit 1
+if [ ${#PASSWDSTAT[@]} -gt 1 ]; then
+    if [ ${array[1]} == 'NP' ]; then
+        passwd root < "${SHED_CONTRIBDIR}/root_passwd"
+    fi
+fi
