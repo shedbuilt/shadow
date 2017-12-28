a523fc4dd2bfa390e9aa4c4204a2f24299c48e77
Auston Stewart <auston@shedbuilt.net>
2017-12-28 13:20:15 -0500

    Temporarily disabling setting of default root password

--- postinstall.sh
+++ postinstall.sh
@@ -3,9 +3,9 @@ pwconv
 grpconv
 sed -i 's/yes/no/' /etc/default/useradd
 # Set root password to default if not set
-read -ra PASSWDSTAT <<< $(passwd --status root) || exit 1
-if [ ${#PASSWDSTAT[@]} -gt 1 ]; then
-    if [ ${array[1]} == 'NP' ]; then
-        passwd root < "${SHED_CONTRIBDIR}/root_passwd"
-    fi
-fi
+# read -ra PASSWDSTAT <<< $(passwd --status root) || exit 1
+# if [ ${#PASSWDSTAT[@]} -gt 1 ]; then
+#    if [ ${PASSWDSTAT[1]} == 'NP' ]; then
+#        passwd root < "${SHED_CONTRIBDIR}/root_passwd"
+#    fi
+# fi
