ab9fefaf9eea1f8e25c3a2041ac79bb024a5c500
Auston Stewart <auston@shedbuilt.net>
2017-12-31 00:21:42 -0500

    Using new approach to set default root password

--- postinstall.sh
+++ postinstall.sh
@@ -3,9 +3,7 @@ pwconv
 grpconv
 sed -i 's/yes/no/' /etc/default/useradd
 # Set root password to default if not set
-# read -ra PASSWDSTAT <<< $(passwd --status root) || exit 1
-# if [ ${#PASSWDSTAT[@]} -gt 1 ]; then
-#    if [ ${PASSWDSTAT[1]} == 'NP' ]; then
-#        passwd root < "${SHED_CONTRIBDIR}/root_passwd"
-#    fi
-# fi
+ROOTSHADOW=$(grep "^root" /etc/shadow)
+if [[ $ROOTSHADOW =~ ^'root:!!:' ]]; then
+    echo 'root:shedbuilt' | chpasswd
+fi
