88006fb6547be2f8ee2752804389f4841e43ca40
Auston Stewart <auston@shedbuilt.net>
2018-01-23 10:31:14 -1000

    Revising to simplify bootstrap

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-if [ "$SHED_BUILDMODE" == 'bootstrap' ]; then
+# Install default passwd and group files, if necessary
+SHEDPKG_INSTALLED_DEFAULTS=false
+if [ ! -e /etc/passwd ]; then
+    install -v -m644 /etc/passwd.default /etc/passwd
+    SHEDPKG_INSTALLED_DEFAULTS=true
+fi
+if [ ! -e /etc/group ]; then
+    install -v -m644 /etc/group.default /etc/group
+    SHEDPKG_INSTALLED_DEFAULTS=true
+fi
+if [ "$SHED_BUILDMODE" == 'bootstrap' ] || $SHEDPKG_INSTALLED_DEFAULTS; then
     pwconv
     grpconv
     sed -i 's/yes/no/' /etc/default/useradd
