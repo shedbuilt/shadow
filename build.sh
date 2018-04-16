4dbef6277232dbeb3cd3c01c54e018e7b423619c
Auston Stewart <auston@shedbuilt.net>
2018-04-16 08:38:41 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sed -i 's/groups$(EXEEXT) //' src/Makefile.in
-find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
-find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
-find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
+sed -i 's/groups$(EXEEXT) //' src/Makefile.in &&
+find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \; &&
+find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \; &&
+find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \; &&
 sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' \
-       -e 's@/var/spool/mail@/var/mail@' etc/login.defs
-sed -i 's/1000/999/' etc/useradd
+       -e 's@/var/spool/mail@/var/mail@' etc/login.defs &&
+sed -i 's/1000/999/' etc/useradd &&
 ./configure --sysconfdir=/etc \
-            --with-group-name-max-length=32 && \
-make -j $SHED_NUM_JOBS && \
-make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-mv -v "${SHED_FAKE_ROOT}/usr/bin/passwd" "${SHED_FAKE_ROOT}/bin"
+            --with-group-name-max-length=32 &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+mv -v "${SHED_FAKE_ROOT}/usr/bin/passwd" "${SHED_FAKE_ROOT}/bin" &&
 # Provide basic user skeleton
 install -v -dm755 "${SHED_FAKE_ROOT}/etc/skel/.config"
--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Install default passwd and group files, if necessary
-SHEDPKG_INSTALLED_DEFAULTS=false
+SHED_PKG_INSTALLED_DEFAULTS=false
 if [ ! -e /etc/passwd ]; then
-    install -v -m644 /etc/passwd.default /etc/passwd
-    SHEDPKG_INSTALLED_DEFAULTS=true
+    install -v -m644 /usr/share/defaults/etc/passwd /etc/passwd
+    SHED_PKG_INSTALLED_DEFAULTS=true
 fi
 if [ ! -e /etc/group ]; then
-    install -v -m644 /etc/group.default /etc/group
-    SHEDPKG_INSTALLED_DEFAULTS=true
+    install -v -m644 /usr/share/defaults/etc/group /etc/group
+    SHED_PKG_INSTALLED_DEFAULTS=true
 fi
-if [ "$SHED_BUILD_MODE" == 'bootstrap' ] || $SHEDPKG_INSTALLED_DEFAULTS; then
+if [ "$SHED_BUILD_MODE" == 'bootstrap' ] || $SHED_PKG_INSTALLED_DEFAULTS; then
     pwconv
     grpconv
     sed -i 's/yes/no/' /etc/default/useradd
