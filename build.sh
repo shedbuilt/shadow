e920584539a9b4e486d39378cc2dfdd1e39aa165
Auston Stewart <auston@shedbuilt.net>
2018-05-30 06:47:36 -1000

    Updating to v4.6

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
 sed -i 's/groups$(EXEEXT) //' src/Makefile.in &&
 find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \; &&
 find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \; &&
@@ -8,8 +10,10 @@ sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@' \
 sed -i 's/1000/999/' etc/useradd &&
 ./configure --sysconfdir=/etc \
             --with-group-name-max-length=32 &&
+# Build and Install
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Rearrange
 mv -v "${SHED_FAKE_ROOT}/usr/bin/passwd" "${SHED_FAKE_ROOT}/bin" &&
-# Provide basic user skeleton
+# Create default user skeleton
 install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/defaults/etc/skel/.config"
--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 # Install default passwd and group files, if necessary
-SHED_PKG_INSTALLED_DEFAULTS=false
+SHED_PKG_LOCAL_INSTALLED_DEFAULTS=false
 if [ ! -e /etc/passwd ]; then
     install -v -m644 /usr/share/defaults/etc/passwd /etc/passwd || exit 1
-    SHED_PKG_INSTALLED_DEFAULTS=true
+    SHED_PKG_LOCAL_INSTALLED_DEFAULTS=true
 fi
 if [ ! -e /etc/group ]; then
     install -v -m644 /usr/share/defaults/etc/group /etc/group || exit 1
-    SHED_PKG_INSTALLED_DEFAULTS=true
+    SHED_PKG_LOCAL_INSTALLED_DEFAULTS=true
 fi
-if [ "$SHED_BUILD_MODE" == 'bootstrap' ] || $SHED_PKG_INSTALLED_DEFAULTS; then
+if [ ! -d /etc/skel ]; then
+    ln -sfv /usr/share/defaults/etc/skel /etc/skel || exit 1
+fi
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ] || $SHED_PKG_LOCAL_INSTALLED_DEFAULTS; then
     pwconv &&
     grpconv &&
     sed -i 's/yes/no/' /etc/default/useradd &&
